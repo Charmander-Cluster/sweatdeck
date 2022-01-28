b0fb0afbda437549d6ae73c3767f00c6fa5979de
Yvette Gong <yvettethea.gong@gmail.com>
2022-01-28 16:36:19 -0500

    created 1. src/store/auth.js; 2. src/components/SignUp.js

---
+++ src/components/SignUp.js
@@ -0,0 +1,136 @@
+import React, { useState } from "react";
+//import firebase from "firebase";
+import { useDispatch } from "react-redux";
+//import { useHistory } from "react-router";
+// import { Redirect } from "react-router-dom";
+import { authSignUp, authenticate } from "../store/auth";
+
+const SignUp = () => {
+  const dispatch = useDispatch();
+  //const history = useHistory();
+  const [userInput, setUserInput] = useState({
+    user: {
+      firstName: "",
+      lastName: "",
+      username: "",
+      password: "",
+      email: "",
+      state: "",
+      birthday: "",
+    },
+  });
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    const formName = evt.target.name;
+    const user = {
+      firstName: evt.target.firstName.value,
+      lastName: evt.target.lastName.value,
+      username: evt.target.username.value,
+      password: evt.target.password.value,
+      email: evt.target.email.value,
+      state: evt.target.state.value,
+      birthday: evt.target.birthday.value,
+    };
+
+    await dispatch(authSignUp(user, formName));
+    await dispatch(authenticate(user.email, user.password));
+  };
+
+  const handleChange = (evt) => {
+    setUserInput({
+      user: { ...userInput.user, [evt.target.name]: evt.target.value },
+    });
+  };
+
+  return (
+    <div className="text-darkest-grey">
+      <h1 className="mb-2 uppercase font-bold text-lg">Sign Up Here</h1>
+      <form className="flex flex-col mb-4" onSubmit={handleSubmit}>
+        <div>
+          {/* <label className="mb-2">First Name: </label> */}
+          <input
+            className="border text-gray-800"
+            type="text"
+            placeholder="First Name"
+            name="firstName"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          {/* <label>Last Name: </label> */}
+          <input
+            className="border text-gray-800"
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          {/* <label>Username: </label> */}
+          <input
+            className="border text-gray-800"
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          {/* <label>Password: </label> */}
+          <input
+            className="border text-gray-800"
+            type="text"
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          {/* <label>E-mail: </label> */}
+          <input
+            className="border text-gray-800"
+            type="text"
+            placeholder="E-mail"
+            name="email"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          {/* <label>State (Location): </label> */}
+          <input
+            className="border text-gray-800"
+            type="text"
+            placeholder="State (location)"
+            name="state"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          {/* <label>Birthday: </label> */}
+          <input
+            className="border text-gray-800"
+            type="text"
+            placeholder="Birthday"
+            name="birthday"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <button className="flex flex-row text-1xl mx-2 my-2 justify-center bg-teal-700 transition duration-150 ease-in-out hover:bg-teal-600 rounded text-white px-8 py-3">
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import UserProfile from "./components/UserProfile";
+import SignUp from './components/SignUp'
 
 const Routes = () => {
   return (
     <div>
       <Switch>
         <Route path="/users/:id" component={UserProfile} />
+        <Route path='/signup' component={SignUp} />
       </Switch>
     </div>
   );
