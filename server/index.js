d0feca80238428acb7f3e1374810bff84c27b1bd
Andrea Khan <khan.andrea@gmail.com>
2022-01-28 17:05:44 -0500

    added initial spotify authentication code

--- server/index.js
+++ server/index.js
@@ -1,2 +1,85 @@
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const passport = require("passport")
+const SpotifyStrategy = require('passport-spotify').Strategy;
+const { default: UserProfile } = require("../src/components/UserProfile")
 
+const app = express()
+app.use(passport.initialize());
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+const SPOTIFY_CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID
+const SPOTIFY_CLIENT_SECRET = process.env.REACT_APP_SPOTIFY_CLIENT_ID
+
+passport.use(
+  new SpotifyStrategy(
+    {
+      clientID: SPOTIFY_CLIENT_ID,
+      clientSecret: SPOTIFY_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/home',
+    },
+    function (accessToken, refreshToken, expires_in, profile, done) {
+      // asynchronous verification, for effect...
+      process.env.REACT_APP_SPOTIFY_TOKEN = accessToken;
+
+      process.nextTick(function () {
+        return done(null, profile);
+
+      });
+    }
+  )
+);
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/auth/spotify', passport.authenticate('spotify'));
+
+app.get(
+  '/auth/spotify',
+  passport.authenticate('spotify', {
+    scope: ['user-read-email', 'user-read-private'],
+    showDialog: true,
+  })
+);
+
+// GET /auth/spotify/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request. If authentication fails, the user will be redirected back to the
+//   login page. Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get(
+  '/auth/spotify/callback',
+  passport.authenticate('spotify', {failureRedirect: '/login'}),
+  function (req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
 
