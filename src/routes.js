052fd0b9e4a2f434d02fbfd4636a08f98fe77ae6
Andrea Khan <khan.andrea@gmail.com>
2022-01-29 14:58:15 -0500

    updated routes for create workout

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import UserProfile from "./components/UserProfile";
 import SignUp from './components/SignUp'
+import CreateWorkout from "./components/CreateWorkout"
 
 const Routes = () => {
   return (
     <div>
       <Switch>
-        <Route path="/users/:id" component={UserProfile} />
+        <Route exact path="/users/:id" component={UserProfile} />
+        <Route exact path="/users/:id/create-workout" component={CreateWorkout} />
         <Route path='/signup' component={SignUp} />
       </Switch>
     </div>
