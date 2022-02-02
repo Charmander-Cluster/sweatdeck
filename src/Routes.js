c58bf9d883bae31b5b125252250ab8bec132ab95
Brandon <cohbrandon@gmail.com>
2022-02-02 15:49:54 -0500

    routes change

--- src/Routes.js
+++ src/Routes.js
@@ -1,66 +1,35 @@
 import React, { useState } from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import UserProfile from "./components/UserProfile";
 import EditUser from "./components/EditUser";
 import SignUp from "./components/SignUp";
-<<<<<<< HEAD:src/routes.js
 import SignIn from "./components/SignIn";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import TestHome from "./components/TestHome";
-=======
 import Dashboard from "./components/Dashboard";
->>>>>>> main:src/Routes.js
 
 const Routes = () => {
-
   // const auth = getAuth();
   // const user = auth.currentUser;
-  
-  const [user, setUser] = useState(getAuth().currentUser)
-  const login = getAuth()
-  onAuthStateChanged(login, (u) => {
-    setUser(u);
-  });
-  
+
+  //   const [user, setUser] = useState(getAuth().currentUser);
+  //   const login = getAuth();
+  //   onAuthStateChanged(login, (u) => {
+  //     setUser(u);
+  //   });
+
   return (
     <div>
-      {/* {console.log('USER:', user)} */}
-      {user ? (
       <Switch>
-        <Route exact path="/" component = {TestHome} />
         <Route path="/users/:id/edit" component={EditUser} />
         <Route exact path="/users/:id" component={UserProfile} />
+        <Route exact path="/" component={Dashboard} />
       </Switch>
-       ):( 
       <Switch>
-        <Route exact path="/" render={() => {
-                return <Redirect to="/signin" />;
-              }}
-            />
         <Route path="/signup" component={SignUp} />
         <Route path="/signin" component={SignIn} />
       </Switch>
-<<<<<<< HEAD:src/routes.js
-       )} 
-=======
-      <Route exact path="/" component={Dashboard} />
->>>>>>> main:src/Routes.js
     </div>
   );
-  
-  // return (
-  //   <div>
-  //     <Switch>
-  //       <Route exact path="/" component = {TestHome} />
-  //       <Route path="/users/:id/edit" component={EditUser} />
-  //       <Route path="/users/:id" component={UserProfile} />
-  //     </Switch>
-  //     <Switch>
-  //       <Route path="/signup" component={SignUp} />
-  //       <Route path="/signin" component={SignIn} />
-  //     </Switch>
-  //   </div>
-  // );
 };
 
 export default Routes;
