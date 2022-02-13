8a1fb3eca46ac744feaadf1d35347575bdbe1537
Andrea Khan <khan.andrea@gmail.com>
2022-02-12 21:45:15 -0500

    updated styling on the cardio playlist component

--- src/components/Spotify/CardioPlaylist.js
+++ src/components/Spotify/CardioPlaylist.js
@@ -102,7 +102,7 @@ const CardioPlaylist = (props) => {
         </h1>
       </div> */}
 
-      <div className="relative z-10 pt-2 pb-5">
+<div className="relative z-10 pt-2 pb-5">
         <div className="container flex flex-col items-start justify-between px-6 mx-auto lg:flex-row lg:items-center">
           <div className="flex flex-col items-start lg:flex-row lg:items-center">
             <div className="my-6 ml-0 lg:ml-20 lg:my-0">
@@ -131,134 +131,151 @@ const CardioPlaylist = (props) => {
                     </div>
                     <div className="flex justify-center">
                       <div className="flex-col justify-center align-center">
-                        <div className="container flex items-center justify-center">
+                        <div className="flex justify-center">
                           <div className="flex justify-center">
-                            {/* <div className="w-20 px-3 md:w-1/2"> */}
-                            <div className="justify-center">
-                              <label
-                                htmlFor="name"
-                                className="block text-sm font-medium"
-                              >
-                                Workout Type
-                              </label>
-                              <select
-                                className="w-64 bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5  dark:placeholder-gray-400 dark:text-purple-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                name="type"
-                                defaultValue=""
-                                value={exercises.type}
-                                onChange={handleNestedChange}
-                              >
-                                <option value="" disabled></option>
-                                <option value="run">Run</option>
-                                <option value="walk">Walk</option>
-                                <option value="swim">Swim</option>
-                                <option value="row">Row</option>
-                                <option value="bike">Bike</option>
-                                <option value="elliptical">Elliptical</option>
-                                <option value="stairs/stair-stepper">
-                                  Stairs/Stair-stepper
-                                </option>
-                                <option value="rollerskate/rollerblade">
-                                  Rollerskate/Rollerblade
-                                </option>
-                              </select>
-                            </div>
+                            <div className=""></div>
                           </div>
                         </div>
+                        <div className="flex align-center">
+                          <div className="flex-col">
 
-                        <div className="container flex justify-center items-center">
-                          <div className="flex-col  justify-center">
-                            <label
-                              htmlFor="name"
-                              className="block mt-2 text-sm font-medium"
-                            >
-                              Name Your Workout
-                            </label>
-                            <input
-                              className="w-72  bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5  dark:placeholder-gray-400 dark:text-purple-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                              required
-                              name="name"
-                              onChange={handleChange}
-                              value={workout.name}
-                            />
-                            <div className="flex mx-5 mt-2 align-center">
-                              <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+
+
+                            <div className="flex-col justify-center items-center">
+
+                              <div className="container flex justify-center">
+                              <div className="flex-col justify-center">
+                              <label
+                                htmlFor="name"
+                                className="block mt-2 text-sm font-medium"
+                              >
+                                Name Your Workout
+                              </label>
+                              <input
+                                className="w-64  bg-gray-50 border border-gray-300 text-purple-600 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2  dark:placeholder-gray-400 dark:text-purple-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                required
+                                name="name"
+                                onChange={handleChange}
+                                value={workout.name}
+                              />
+                              </div>
+                              </div>
+
+                              <div className="container flex justify-center">
+                                <div className="flex justify-center">
+                                  {/* <div className="w-20 px-3 md:w-1/2"> */}
+                                  <div className="">
+                                    <label
+                                      htmlFor="name"
+                                      className="block mt-2 text-sm font-medium"
+                                    >
+                                      Workout Type
+                                    </label>
+                                    <select
+                                      className="w-64 bg-gray-50 border border-gray-300 text-purple-600 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2  dark:placeholder-gray-400 dark:text-purple-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                      name="type"
+                                      defaultValue=""
+                                      value={exercises.type}
+                                      onChange={handleNestedChange}
+                                    >
+                                      <option value="" disabled></option>
+                                      <option value="run">Run</option>
+                                      <option value="walk">Walk</option>
+                                      <option value="swim">Swim</option>
+                                      <option value="row">Row</option>
+                                      <option value="bike">Bike</option>
+                                      <option value="elliptical">
+                                        Elliptical
+                                      </option>
+                                      <option value="stairs/stair-stepper">
+                                        Stairs/Stair-stepper
+                                      </option>
+                                      <option value="rollerskate/rollerblade">
+                                        Rollerskate/Rollerblade
+                                      </option>
+                                    </select>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+
+                            <div className="flex justify-center">
+                              {/* <div className="w-20 md:w-1/2 px-3"> */}
+                              <div className="">
                                 <label
-                                  htmlFor="distance"
-                                  className="block text-sm font-medium"
+                                  htmlFor="weight"
+                                  className="block text-sm font-medium mt-3"
                                 >
                                   Distance
                                 </label>
                                 <input
+                                  className="w-16 bg-gray-50 border border-gray-300 text-purple-600 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 mx-1  dark:placeholder-gray-400 dark:text-purple-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                   name="distance"
-                                  className="w-28 @error bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 m-1 dark:placeholder-gray-400 dark:text-purple-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                   required
                                   type="number"
                                   min="0"
                                   onChange={handleNestedChange}
-                                  value={exercises.distance}
+                                  value={workout.distance}
                                 />
                               </div>
 
-                              <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                              <div className="">
                                 <label
                                   htmlFor="units"
-                                  className="block text-sm font-medium"
+                                  className="block text-sm font-medium mt-3"
                                 >
                                   Units
                                 </label>
                                 <select
-                                  className="w-28 h-12 text-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 m-1  dark:placeholder-gray-400 dark:text-purple-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                  className="w-16 mr-4 h-12 bg-gray-50 border border-gray-300 text-purple-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 mx-1 dark:placeholder-gray-400 dark:text-purple-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                   name="units"
-                                  required
                                   defaultValue="select"
-                                  value={exercises.units}
                                   onChange={handleNestedChange}
+                                  value={workout.units}
                                 >
                                   <option value="select" disabled>
-                                    Select
+                                    --
                                   </option>
-                                  <option value="miles">miles</option>
-                                  <option value="kilometers">kilometers</option>
-                                  <option value="meters">meters</option>
-                                  <option value="yards">yards</option>
+                                  <option value="miles">mi</option>
+                                  <option value="kilometers">km</option>
+                                  <option value="meters">m</option>
+                                  <option value="yards">yds</option>
                                 </select>
                               </div>
-                            </div>
-                            <div className="container flex justify-center">
-                              <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+
+                              <div className="">
                                 <label
                                   htmlFor="hours"
-                                  className="block text-sm font-medium"
+                                  className="block text-sm font-medium mt-3"
                                 >
                                   Hours
                                 </label>
                                 <input
+                                  className="w-16 bg-gray-50 border border-gray-300 text-purple-600 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 mx-1  dark:placeholder-gray-400 dark:text-purple-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                   name="hours"
-                                  className="w-20 bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 m-1 dark:placeholder-gray-400 dark:text-purple-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                  min="0"
                                   required
                                   type="number"
-                                  min="0"
-                                  value={exercises.hours}
                                   onChange={handleNestedChange}
+                                  value={workout.hours}
                                 />
                               </div>
-                              <div className="mt-10">:</div>
-                              <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+
+                              <div className="mt-12">:</div>
+                              <div className="">
                                 <label
                                   htmlFor="minutes"
-                                  className="block text-sm font-medium "
+                                  className="block text-sm font-medium mt-3"
                                 >
                                   Minutes
                                 </label>
                                 <input
+                                  className="w-16 bg-gray-50 border border-gray-300 text-purple-600 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 mx-1 dark:placeholder-gray-400 dark:text-purple-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                   name="minutes"
-                                  className="w-20 bg-gray-50 border border-gray-300 text-purple-800 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 m-1 dark:placeholder-gray-400 dark:text-purple-800 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                  required
                                   type="number"
                                   min="0"
-                                  value={exercises.minutes}
                                   onChange={handleNestedChange}
+                                  value={workout.minutes}
                                 />
                               </div>
                             </div>
