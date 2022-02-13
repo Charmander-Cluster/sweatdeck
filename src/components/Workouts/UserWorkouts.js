4cee382be2f6b7a51bb83b265c2f0481b23c4e00
Brandon <cohbrandon@gmail.com>
2022-02-13 13:09:02 -0500

    header higher up

--- src/components/Workouts/UserWorkouts.js
+++ src/components/Workouts/UserWorkouts.js
@@ -63,8 +63,8 @@ const UserWorkouts = (props) => {
       </div>
     </div>
   ) : (
-    <div className="container flex flex-col w-screen py-2">
-      <div className="relative z-10 pt-2 pb-10">
+    <div className="container flex flex-col w-screen">
+      <div className="relative z-10 pb-10 md:mt-4">
         <div className="container flex flex-col items-start justify-between px-6 mx-auto lg:flex-row lg:items-center">
           <div className="flex flex-col items-start lg:flex-row lg:items-center">
             <div className="my-6 ml-0 lg:ml-20 lg:my-0">
@@ -77,7 +77,7 @@ const UserWorkouts = (props) => {
         </div>
       </div>
 
-      <div className="pt-20 overflow-hidden rounded">
+      <div className="pt-5 overflow-hidden rounded">
         <div className="grid grid-cols-1">
           {workouts.map((workout) => {
             return (
