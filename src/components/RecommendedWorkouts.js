8d7a4b38072094122b29018fc639ba66fa603f58
israellund <israelmlund@gmail.com>
2022-02-13 18:58:00 -0500

    tweaked Your Workouts and Recommended Workouts to be the same

--- src/components/RecommendedWorkouts.js
+++ src/components/RecommendedWorkouts.js
@@ -18,51 +18,46 @@ const RecommendedWorkouts = (props) => {
   }, [dispatch, id, group, cardioOrStrength]);
 
   return (
-  <div>
     <div className="container flex flex-col items-center justify-center w-screen p-3 py-2">
       <div className="flex items-center justify-center">
-        <div className="grid grid-cols-1">
-
-          <div className="pt-20 overflow-hidden rounded">
-            <div className="grid grid-cols-1">
-              {recommendedWorkouts.map((workout) => {
-                return (
-                  <div
-                    key={workout.elemId}
-                    className="flex flex-row justify-center w-full -mt-10 text-1xl"
-                  >
-                    <div className="m-3 my-5 overflow-x-auto border border-teal-500 rounded-md bg-neutral-700 mb-14">
-                      <div className="justify-center max-w-4xl p-3 ">
-                        <div className="flex flex-wrap -mx-3">
-                          <div className="container flex justify-center w-screen text-center">
-                            <div className="block m-3 text-2xl font-bold">
-                              <div>
-                                <div className="m-1 text-2xl font-bold uppercase">
-                                  {workout.elemData.name}
-                                </div>
-
-                                <Link
-                                  to={`/users/${id}/recommendedWorkouts/${workout.elemId}`}
-                                  className="p-3 text-sm text-center text-white bg-teal-500 rounded-md"
-                                >
-                                  See Workout
-                                </Link>
+        <div className="pt-5 overflow-hidden rounded">
+          <div className="grid grid-cols-1">
+            {recommendedWorkouts.map((workout) => {
+              return (
+                <div
+                  key={workout.elemId}
+                  className="flex flex-row justify-center w-full -mt-10 text-1xl"
+                >
+                  <div className="m-3 my-5 overflow-x-auto border border-teal-500 rounded-md bg-neutral-700 mb-14">
+                    <div className="justify-center max-w-4xl p-3 ">
+                      <div className="flex flex-wrap -mx-3">
+                        <div className="container flex justify-center w-screen text-center">
+                          <div className="block m-3 text-2xl font-bold">
+                            <div>
+                              <div className="m-1 text-2xl font-bold uppercase">
+                                {workout.elemData.name}
                               </div>
+
+                              <Link
+                                to={`/users/${id}/recommendedWorkouts/${workout.elemId}`}
+                                className="p-3 text-sm text-center text-white bg-teal-500 rounded-md"
+                              >
+                                See Workout
+                              </Link>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                );
-              })}
-            </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
     </div>
-  </div>
-  )
+  );
 };
 
 export default RecommendedWorkouts;
--- src/components/Workouts/UserWorkouts.js
+++ src/components/Workouts/UserWorkouts.js
@@ -3,7 +3,7 @@ import { fetchUserWorkoutsThunk } from "../../store/workoutsPage";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import RecommendedWorkouts from "../RecommendedWorkouts"
+import RecommendedWorkouts from "../RecommendedWorkouts";
 
 const UserWorkouts = (props) => {
   const workouts = useSelector((state) => state.allWorkouts);
@@ -94,40 +94,44 @@ const UserWorkouts = (props) => {
             </div>
           </div>
 
-          <div className="pt-5 overflow-hidden rounded">
-            <div className="grid grid-cols-1">
-              {workouts.map((workout) => {
-                return (
-                  <div
-                    key={workout.elemId}
-                    className="flex flex-row justify-center w-full -mt-10 text-1xl"
-                  >
-                    <div className="m-3 my-5 overflow-x-auto border border-teal-500 rounded-md bg-neutral-700 mb-14">
-                      <div className="justify-center max-w-4xl p-3 ">
-                        <div className="flex flex-wrap -mx-3">
-                          <div className="container flex justify-center w-screen text-center">
-                            <div className="block m-3 text-2xl font-bold">
-                              <div>
-                                <div className="m-1 text-2xl font-bold uppercase">
-                                  {workout.elemData.name}
-                                </div>
+          <div className="container flex flex-col items-center justify-center w-screen p-3 py-2">
+            <div className="flex items-center justify-center">
+              <div className="pt-5 overflow-hidden rounded">
+                <div className="grid grid-cols-1">
+                  {workouts.map((workout) => {
+                    return (
+                      <div
+                        key={workout.elemId}
+                        className="flex flex-row justify-center w-full -mt-10 text-1xl"
+                      >
+                        <div className="m-3 my-5 overflow-x-auto border border-teal-500 rounded-md bg-neutral-700 mb-14">
+                          <div className="justify-center max-w-4xl p-3 ">
+                            <div className="flex flex-wrap -mx-3">
+                              <div className="container flex justify-center w-screen text-center">
+                                <div className="block m-3 text-2xl font-bold">
+                                  <div>
+                                    <div className="m-1 text-2xl font-bold uppercase">
+                                      {workout.elemData.name}
+                                    </div>
 
-                                <Link
-                                  to={`/users/${id}/workouts/${workout.elemId}`}
-                                  // onClick={handleSubmitWithSpotify}
-                                  className="p-3 text-sm text-center text-white bg-teal-500 rounded-md"
-                                >
-                                  See Workout
-                                </Link>
+                                    <Link
+                                      to={`/users/${id}/workouts/${workout.elemId}`}
+                                      // onClick={handleSubmitWithSpotify}
+                                      className="p-3 text-sm text-center text-white bg-teal-500 rounded-md"
+                                    >
+                                      See Workout
+                                    </Link>
+                                  </div>
+                                </div>
                               </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                );
-              })}
+                    );
+                  })}
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -155,14 +159,10 @@ const UserWorkouts = (props) => {
                   Recommended {upperCaseType} Workouts
                 </button>
                 <div className="h-1 mt-4 rounded-full bg-gradient-to-l from-teal-600 to-purple-600"></div>
-
               </div>
             </div>
           </div>
-
-        <RecommendedWorkouts 
-        cardioOrStrength = {cardioOrStrength}
-       />
+          <RecommendedWorkouts cardioOrStrength={cardioOrStrength} />
         </div>
       )}
     </div>
